4c8172c896627092a6f789a6c039a1ab178950ba
soyeon <gkdl0471@gmail.com>
2026-01-23 02:06:48 +0900

    feat: 쪽지시험 응시하기 페이지 기본 UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router'
 import '@/App.css'
 import LandingPage from '@/pages/LandingPage'
 import TestPage from '@/pages/TestPage'
 import { Header } from '@/components/common'
 import MyPage from '@/pages/MyPage'
 import MyPageQuiz from '@/components/MyPageQuiz'
+import QuizPage from './pages/QuizPage'
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  // 퀴즈 페이지일 때는 공통 Header를 숨깁니다.
+  const showHeader = location.pathname !== '/quiz';
+
   return (
-    <BrowserRouter>
-      <Header />
+    <>
+      {showHeader && <Header />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/qna" element={<div>질의응답 페이지 (구현 예정)</div>} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/quiz" element={<QuizPage />} />
 
         {/* 마이페이지 및 하위컴포넌트 연결 */}
         <Route path="/mypage" element={<MyPage />}>
@@ -22,8 +28,17 @@ function App() {
           <Route path="password" element={<div>비밀번호 변경</div>} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   )
+}
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
 }
 
 export default App
