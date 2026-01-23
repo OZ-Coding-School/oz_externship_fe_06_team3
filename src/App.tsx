bdb56a1f8a99240785ba2cf58c1d3fdde03b2253
soyeon <gkdl0471@gmail.com>
2026-01-23 11:25:58 +0900

    refactor: Outlet 기반의 중첩 라우팅 구조로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,33 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import '@/App.css'
 import LandingPage from '@/pages/LandingPage'
 import TestPage from '@/pages/TestPage'
-import { Header } from '@/components/common'
 import MyPage from '@/pages/MyPage'
 import MyPageQuiz from '@/components/MyPageQuiz'
 import QuizPage from './pages/QuizPage'
-
-function AppContent() {
-  const location = useLocation();
-  // 퀴즈 페이지일 때는 공통 Header를 숨깁니다.
-  const showHeader = location.pathname !== '/quiz';
-
-  return (
-    <>
-      {showHeader && <Header />}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/qna" element={<div>질의응답 페이지 (구현 예정)</div>} />
-        <Route path="/test" element={<TestPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-
-        {/* 마이페이지 및 하위컴포넌트 연결 */}
-        <Route path="/mypage" element={<MyPage />}>
-          <Route path="quiz" element={<MyPageQuiz />} />
-          <Route path="profile" element={<div>내 정보</div>} />
-          <Route path="password" element={<div>비밀번호 변경</div>} />
-        </Route>
-      </Routes>
-    </>
-  )
-}
-
+import MainLayout from './components/layout/MainLayout'
 
 function App() {
   return (
     <BrowserRouter>
-      <AppContent />
+      <Routes>
+        {/* 1. 헤더가 포함된 레이아웃을 적용할 페이지들 */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/qna" element={<div>질의응답 페이지</div>} />
+          <Route path="/test" element={<TestPage />} />
+          
+          {/* 마이페이지와 그 하위 페이지 */}
+          <Route path="/mypage" element={<MyPage />}>
+            <Route path="quiz" element={<MyPageQuiz />} />
+            <Route path="profile" element={<div>내 정보</div>} />
+            <Route path="password" element={<div>비밀번호 변경</div>} />
+          </Route>
+        </Route>
+
+        {/* 2. 헤더가 필요 없는 페이지 (레이아웃 밖으로 배치) */}
+        <Route path="/quiz" element={<QuizPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
