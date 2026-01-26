7ad4fdce3545775d593c55d96cace743cefe3a9e
agrade1 <kigf59@gmail.com>
2026-01-26 15:37:59 +0900

    feat: 쪽지시험 관련 핸들러 묶음

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,14 @@
 
 import { http, HttpResponse } from 'msw'
-import { checkCodeHandler } from './handlers/checkCode'
-import { examDeploymentDetailHandler } from './handlers/examDeploymentDetail'
-import { examDeploymentStatusHandler } from './handlers/examDeploymentStatus'
-import { examDeploymentsHandler } from './handlers/examDeployments'
-import { examSubmissionHandler } from './handlers/examSubmission'
-import { examSubmissionResultHandler } from './handlers/examSubmissionResult'
+import { checkCodeHandler } from './handlers/quiz/checkCode'
+import { examDeploymentDetailHandler } from './handlers/quiz/examDeploymentDetail'
+import { examDeploymentStatusHandler } from './handlers/quiz/examDeploymentStatus'
+import { examDeploymentsHandler } from './handlers/quiz/examDeployments'
+import { examSubmissionHandler } from './handlers/quiz/examSubmission'
+import { examSubmissionResultHandler } from './handlers/quiz/examSubmissionResult'
 
 export const helloHandler = http.get('/api/hello', () => {
-    return HttpResponse.json({ message: 'Hello, world!', code: 200 })
+  return HttpResponse.json({ message: 'Hello, world!', code: 200 })
 })
 
 export const handlers = [
