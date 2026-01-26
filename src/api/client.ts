cfa29dee605bb9977b3946259a89633a717c6f5c
JinMyeong Choi <ymo6538@gmail.com>
2026-01-26 11:53:25 +0900

    feat: axios client 세팅

---
+++ src/api/client.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { useAuthStore } from '@/store/authStore'
+
+export const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? '',
+  withCredentials: true,
+})
+
+apiClient.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().accessToken
+  if (token) {
+    config.headers = config.headers ?? {}
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
